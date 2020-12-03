f936aced3eb5ed91b01df93ae6a60aabba3b011a
yawar2019 <yawarali17@gmail.com>
2020-12-03 08:19:19 +0530

    committed code

--- CodeFirstApproach/Controllers/DefaultController.cs
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Data.Entity;
 namespace CodeFirstApproach.Controllers
 {
     public class DefaultController : Controller
@@ -40,6 +40,59 @@ namespace CodeFirstApproach.Controllers
                 return View();
 
             }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id) {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State = EntityState.Modified;//update Query
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+
+            }
+
+            
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            db.EmployeeModels.Remove(emp);
+            int i = db.SaveChanges();
+            if (i > 0)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+
+            }
+
+
         }
     }
 }
